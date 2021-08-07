e37edd14f59caf566a8ac8000612f5f81a301c7f
Bread <seanthorne8@gmail.com>
2021-08-07 11:09:46 +0100

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# discordtimestamps
+
+A simple Vue 3.0 project to learn the new api, the Discord timestamps allow you to format timestamps in your local format.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+<header>
+  <h1>Discord timestamp formatter</h1>
+  <p>Choose a date from the picker and have it displayed local to you in Discord</p>
+  </header>
+<main>
+  <Home/>
+</main>
+</template>
+
+<script>
+import Home from "./pages/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+body{
+  background: #36393f;
+  color: #dcddde;
+font-family: 'Roboto', sans-serif;  font-weight: 400;
+}
+h1, h2, h3{
+}
+main{
+  max-width: 900px;
+  margin: 0 auto;
+}
+header{
+    max-width: 900px;
+  margin: 0 auto;
+  padding-top: 4rem;
+  text-align: center;
+}
+</style>
